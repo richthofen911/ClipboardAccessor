42ba4c4ee53142c282eeb4d5c53976dee295ba3b
richthofen911 <yichaoli.richthofen@gmail.com>
2015-11-13 12:47:15 -0500

    be able to set clipboard data

---
+++ app/src/main/java/net/callofdroidy/clipboardaccessor/ServiceClipboardAccessor.java
@@ -0,0 +1,26 @@
+package net.callofdroidy.clipboardaccessor;
+
+import android.app.Service;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ServiceClipboardAccessor extends Service {
+    public ServiceClipboardAccessor() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        ((ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText("label::textFromOutside", intent.getStringExtra("text")));
+        stopSelf();
+        return START_NOT_STICKY;
+    }
+}
